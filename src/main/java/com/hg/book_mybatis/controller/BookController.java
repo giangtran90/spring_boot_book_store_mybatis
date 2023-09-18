7308260d08d2adf9b6a5265bff6eadddd1aec634
ADMIN <giang08t3@gmail.com>
2023-09-18 11:19:17 +0900

    Optimize trang home + hiện thị page create + list +redirect qua page available

--- src/main/java/com/hg/book_mybatis/controller/BookController.java
+++ src/main/java/com/hg/book_mybatis/controller/BookController.java
@@ -2,6 +2,10 @@ package com.hg.book_mybatis.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.hg.book_mybatis.entity.BookEntity;
 
 @Controller
 public class BookController {
@@ -9,5 +13,21 @@ public class BookController {
 	@GetMapping("/")
 	public String displayHome() {
 		return "home";
+	}
+	
+	@GetMapping("/available_books")
+	public String displayAvailableBooks() {
+		return "books/bookList";
+	}
+	
+	@GetMapping("/book_register")
+	public String displayBookRegister() {
+		return "books/bookRegister";
+	}
+	
+	@PostMapping("/save")
+	public String saveBook(@ModelAttribute BookEntity bookEntity) {
+		
+		return "redirect:/available_books";
 	}
 }
