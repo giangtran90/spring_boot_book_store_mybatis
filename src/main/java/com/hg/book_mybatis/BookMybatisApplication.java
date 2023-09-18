62945150b751a414557dd09a7b18e62b4e1030f4
ADMIN <giang08t3@gmail.com>
2023-09-18 09:00:01 +0900

    init project bookstore with mybatis frame work

---
+++ src/main/java/com/hg/book_mybatis/BookMybatisApplication.java
@@ -0,0 +1,13 @@
+package com.hg.book_mybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookMybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookMybatisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hg/book_mybatis/BookMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hg.book_mybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookMybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
