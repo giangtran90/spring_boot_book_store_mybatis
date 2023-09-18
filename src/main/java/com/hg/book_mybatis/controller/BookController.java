770e9849a2a92e0aa18a42b1055e9d052de228a4
ADMIN <giang08t3@gmail.com>
2023-09-18 14:57:31 +0900

    Thực hiện validation cho form create: thêm dependency validation + sử dụng anotation để validate ở dto + dung controller kiểm tra điều kiện + lấy lỗi hiện thị ra frontend ngoài màn hình

--- src/main/java/com/hg/book_mybatis/controller/BookController.java
+++ src/main/java/com/hg/book_mybatis/controller/BookController.java
@@ -2,6 +2,9 @@ package com.hg.book_mybatis.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,7 +34,11 @@ public class BookController {
 	}
 	
 	@PostMapping("/save")
-	public String saveBook(@ModelAttribute BookAddRequest bookAddRequest) {
+	public String saveBook(@Validated @ModelAttribute BookAddRequest bookAddRequest, BindingResult bindingResult, Model model) {
+		if (bindingResult.hasErrors()) {
+			model.addAttribute("errors", bindingResult.getAllErrors());
+			return "books/bookRegister";
+		}
 		bookService.save(bookAddRequest);
 		return "redirect:/available_books";
 	}
--- src/main/java/com/hg/book_mybatis/dto/BookAddRequest.java
+++ src/main/java/com/hg/book_mybatis/dto/BookAddRequest.java
@@ -1,10 +1,18 @@
 package com.hg.book_mybatis.dto;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 @Data
 public class BookAddRequest {
+	@NotEmpty(message = "名前を入力してください")
+	@Size(max = 100, message = "本名は100桁以内で入力してください")
 	private String name;
+	@Size(max = 100, message = "作者は100桁以内で入力してください")
 	private String author;
+	@Pattern(regexp = "^[0-9]*",message = "金額は数（０－９）を入力してください")
 	private String price;
 }
