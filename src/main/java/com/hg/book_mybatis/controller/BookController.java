4bbb18746e4badb5de47cc4fba07d98b9d9c0155
ADMIN <giang08t3@gmail.com>
2023-09-18 09:18:39 +0900

    config file application.properties + tạo page home và controller để hiển thị page home ra màn hình

---
+++ src/main/java/com/hg/book_mybatis/controller/BookController.java
@@ -0,0 +1,13 @@
+package com.hg.book_mybatis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+	@GetMapping("/")
+	public String displayHome() {
+		return "home";
+	}
+}
